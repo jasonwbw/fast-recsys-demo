de1bdf0c20f3ce357e0d27ac112915e26091adec
jasonwbw <384444165@163.com>
2013-11-06 18:12:10 +0800

    fisrt try to build some extendibility

--- controllers/ad.py
+++ controllers/ad.py
@@ -11,8 +11,26 @@ sys.setdefaultencoding('utf-8')
 render = settings.render
 
 class ContentIndex:
-    def GET(self):        
-        return render.index("test_title", "test_content", "recommend total", \
-                            [{'text':"text1","img":"images/pic06.jpg"}, {'text':"text2","img":"images/pic06.jpg"}, \
-                             {'text':"text3","img":"images/pic06.jpg"}, {'text':"text4","img":"images/pic06.jpg"}], \
-                            "no other info", "http://demo")
+    def GET(self):       
+        content_title, content_text = self._getContent()
+        rec_total, recs, other_info = self._get_rec(content_title, content_text)
+        feedback_url = "./"
+        return render.index(content_title, content_text,\
+        	rec_total, recs, \
+        	other_info, feedback_url)
+
+    def _get_content(self):
+    	content_title = "test_title" 
+        content_text = "test_content"
+        return content_title, content_text
+
+    def _get_rec(self, content_title, content_text):
+    	rec_total = "recommend total"
+        recs = [{'text':"text1","img":"images/pic06.jpg"}, {'text':"text2","img":"images/pic06.jpg"}, \
+                {'text':"text3","img":"images/pic06.jpg"}, {'text':"text4","img":"images/pic06.jpg"}]
+        other_info = "no other info"
+        return rec_total, recs, other_info
+
+class Feedback:
+    def POST(self, title, recid, message):   
+    	pass
