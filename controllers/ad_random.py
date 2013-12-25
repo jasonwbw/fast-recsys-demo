af4ed34eab24c814f263ba92e7f81adbb7d4eb0f
jasonwbw <384444165@163.com>
2013-12-25 11:38:12 +0800

    edit to common demo

--- controllers/ad_random.py
+++ controllers/ad_random.py
@@ -4,53 +4,37 @@
 import web
 from config import settings
 
-import os
-import random
-import linecache
-from lib.ads import ADs
-from lib.buildvec_online import VecBuilderOnline
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 render = settings.render
 
 # global attribute
-base = '/home/adrd-dev/nlp_flow/code'
-builder = VecBuilderOnline(base + "/data/idf", base + "/data/pairs", \
-    base + "/data/kw", base + "/data/kw_pmi_app", base + "/data/kw_w2v_app")
-ads = ADs(base + '/all_ad_c_vec', \
-    base + '/all_ad_vec')
-basepath = base + '/data/output/'
-segedbasepath = base + '/seged/output/'
+_id = 1
 
 class ContentIndex: 
 
     def GET(self):
-        global builder
-        news_c = random.choice(builder.all_news_c())
-        line = random.randint(1, 50)
-        content_title, content_text = self._get_content(attrs = {'news_c':news_c, 'line':line})
-        rec_total, recs, other_info = self._get_rec(attrs = {'news_c':news_c, 'line':line})
+        global _id
+        _id += 1
+        print '_id' + str(_id)
+        content_title, content_text = self._get_content()
+        rec_total, recs, other_info = self._get_rec(content_title, content_text)
         feedback_url = "./feedback_base_ad"
         return render.index(content_title, content_text,\
-        	rec_total, recs, \
-        	other_info, feedback_url)
+           rec_total, recs, \
+           other_info, feedback_url)
 
-    def _get_content(self, attrs = None):
-        global basepath
-    	content_title = attrs['news_c']
-        content_text = linecache.getline(basepath+attrs['news_c']+'/news', attrs['line'])
+    def _get_content(self):
+       content_title = "test_title" 
+        content_text = "test_content"
         return content_title, content_text
 
-    def _get_rec(self, attrs = None):
-        global basepath, segedbasepath, builder, ads
-        news = linecache.getline(segedbasepath+attrs['news_c']+'/news', attrs['line'])
-        vec = builder.build_news_tfidf(attrs['news_c'], news.split(" "))
-        items = ads.find_ad(2, 5, vec, attrs['news_c'])
-    	rec_total = ""
-        recs = []
-        for ad_c, item in items:
-            rec_total += ad_c + " "
-            recs.append({'text': linecache.getline(basepath+attrs['news_c']+'/'+ad_c, int(item)).strip(), \
-                "img":" "})
+    def _get_rec(self, content_title, content_text):
+       rec_total = "recommend total"
+        recs = [{'text':"text1","img":"images/pic06.jpg"}, {'text':"text2","img":"images/pic06.jpg"}, \
+                {'text':"text3","img":"images/pic06.jpg"}, {'text':"text4","img":"images/pic06.jpg"}]
         other_info = "no other info"
         return rec_total, recs, other_info
 
@@ -61,4 +45,4 @@ class Feedback:
         self.handle_feedback_data(data)
 
     def handle_feedback_data(self, data):
-    	return 'feed back', 'title:', data['title'], ', recid:', data['recid'], ', message:', data['message']
+       return 'feed back', 'title:', data['title'], ', recid:', data['recid'], ', message:', data['message']
