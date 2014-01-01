0674931a6754e2db2432e4053f7735aef5505af5
jasonwbw <384444165@163.com>
2014-01-01 23:59:44 +0800

    add a choosed content demo controller

---
+++ controllers/ad_choose.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import web
+from config import settings
+from recommend.recommend_center import ChooseController
+
+render = settings.render
+
+# global attribute
+controller = ChooseController()
+
+class ContentIndex: 
+
+    def GET(self, page = None):
+        global controller
+        page_num = page and int(page) or 0
+        content, ads = controller.get_content_ads(page_num)
+        content_title, content_text = self._get_content(content = content)
+        rec_total, recs, other_info = self._get_rec(ads = ads)
+        feedback_url = "./feedback_base_ad"
+        item_contain_img = False
+        return render.ads_choose(page_num, content_title, content_text,\
+        	rec_total, recs, \
+        	other_info, feedback_url, item_contain_img)
+
+    def _get_content(self, **args):
+    	content = args['content']
+        content_title = content[0]
+        content_text = content[1]
+        return content_title, content_text
+
+    def _get_rec(self, **args):
+    	rec_total = ""
+        recs = []
+        for rec in args['ads']:
+            recs.append({'text' : rec, 'img' : ''})
+        for i in xrange(3 - len(recs) % 3):
+            recs.append({'text' : '', 'img' : ''})
+        other_info = "no other info"
+        return rec_total, recs, other_info
+
+class Feedback:
+
+    def POST(self):   
+        data = web.input()
+        self.handle_feedback_data(data)
+
+    def handle_feedback_data(self, data):
+    	return 'feed back', 'title:', data['title'], ', recid:', data['recid'], ', message:', data['message']
