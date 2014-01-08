6bc54e1d5215e0dee262847d99887ba616526caf
jasonwbw <384444165@163.com>
2014-01-08 18:47:14 +0800

    add new control center for retirval

--- controllers/recommend/recommend_center.py
+++ controllers/recommend/recommend_center.py
@@ -7,10 +7,64 @@ import ConfigParser
 from tools import builder, distance
 from bean import loader
 from ranking.ranking_strategys import Ranking
+from retrival.category_retrival_strategys import CategoryRetrivalStrategyCenter
+from abc import ABCMeta, abstractmethod
 
 class ChooseController(object):
 
 	'''Used for demo, other demo can extends this class
+	'''
+
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def __init__(self, **args):
+		pass
+
+	@abstractmethod
+	def load_conf(self):
+		'''Read config file and build builder, distance and loader'''
+		pass
+
+	@abstractmethod
+	def build_vec_getter(self):
+		'''Build the ad and content vector getter
+		
+		Returns:
+			tuple of ad vector getter and content vector getter
+		'''
+		pass
+
+	@abstractmethod
+	def number_content(self):
+		'''Give all content number and save the key'''
+		pass
+
+	@abstractmethod
+	def content_count(self):
+		'''Count the content number
+
+		Returns:
+			the result number
+		'''
+		pass
+
+	@abstractmethod
+	def get_content_ads(self, num):
+		'''Get the content whose id is num and the recommend ads
+
+		Args:
+			num : content id
+
+		Returns:
+			(content, [list of ad content])
+		'''
+		pass
+
+
+class RankingChooseController(ChooseController):
+
+	'''Just use the ranking process.
 	'''
 
 	def __init__(self):
@@ -85,6 +139,47 @@ class ChooseController(object):
 			return None
 		content_obj = self.loader.load_content(self.content_key[num])
 		ads = self.ranking.ranking(content_obj, self.ad_cache, topk = 2)
+		res_ad = []
+		for score, ad_key in ads:
+			res_ad.append(self.loader.get_ad_info(ad_key))
+		return (self.loader.get_content_info(self.content_key[num]), res_ad)
+
+
+class RetrivalAndRankingChooseController(RankingChooseController):
+
+	'''Use the retrival ad category firstly, and ranking the ads.
+	'''
+
+	def __init__(self):
+		self.load_conf()
+		getters = self.build_vec_getter()
+
+		self.ranking = Ranking(self.builder, self.distance, getters[0], getters[1])
+		self.ad_c_cache, self.ad_cache = self.loader.load_ads()
+
+		self.number_content()
+		self.retrival = CategoryRetrivalStrategyCenter(builder = self.builder, distance = self.distance)
+
+	def get_content_ads(self, num):
+		'''Get the content whose id is num and the recommend ads
+
+		Args:
+			num : content id
+
+		Returns:
+			(content, [list of ad content])
+		'''
+		if num >= self.count:
+			return None
+		content_obj = self.loader.load_content(self.content_key[num])
+		
+		categorys = self.retrival.get_category(content_obj, self.ad_c_cache)
+		retrival_ads = []
+		for category in categorys:
+			retrival_ads += self.ad_cache.get_ads(category)
+		print retrival_ads
+		
+		ads = self.ranking.ranking(content_obj, retrival_ads, topk = 2)
 		res_ad = []
 		for score, ad_key in ads:
 			res_ad.append(self.loader.get_ad_info(ad_key))
